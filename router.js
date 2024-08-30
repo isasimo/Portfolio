428e5766cbd82364946b72773a359d7ee209014c
Isa <isasimoaynes@gmail.com>
2024-08-30 13:48:44 +0200

    Added project template

--- router.js
+++ router.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from './components/Home.vue';
 import About from './components/About.vue';
 import ProjectConstructor from './components/ProjectConstructor.vue';
+import ProjectTemplate from './components/ProjectTemplate.vue';
 /* import Project2 from './components/Project2.vue'; */
 
 const routes = [
@@ -11,6 +12,11 @@ const routes = [
     { path: '/about', component: About },
     { path: '/constructor', component: ProjectConstructor },
     /*      { path: '/project2', component: Project2 }, */
+
+    /* ONLY FOR PREVIEW
+    Comment for production */
+    { path: '/project-template', component: ProjectTemplate },
+
 ];
 
 const router = createRouter({
